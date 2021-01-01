49bf5e6b1526d4c23ed128292c91b2303d308537
Al Avery <al.avery.dev@gmail.com>
2021-01-01 16:02:47 -0600

    inotifywait version

--- code/process_zoom_directory_loop.py
+++ code/process_zoom_directory_loop.py
@@ -1,7 +1,8 @@
+print('python invoked')
 import sys
 import json
 import os
-from read_settings import get_all
+from read_settings import get_value
 
 
 def watch_new_dir(dir_name, zoom_base_dir, max_seconds_to_wait):
@@ -9,7 +10,7 @@ def watch_new_dir(dir_name, zoom_base_dir, max_seconds_to_wait):
     from upload import upload_video
 
     new_dir_path = os.path.join(zoom_base_dir, dir_name)
-    print("Monitoring {dir} for mp4 file".format(new_dir_path))
+    print("Monitoring \"{dir}\" for mp4 file".format(dir=new_dir_path))
     
     # every 10 seconds, see if an mp4 file exists in the newly created directory
     POLL_INTERVAL_SECONDS = 10
@@ -17,9 +18,7 @@ def watch_new_dir(dir_name, zoom_base_dir, max_seconds_to_wait):
     while seconds_waited < max_seconds_to_wait:
         video_files = get_video_files(new_dir_path)
         if len(video_files) > 0:
-            print("Saw .mp4 file :)")
-            # name the Gdrive file as the directory
-            return upload_video(os.path.join(new_dir_path, video_files[0]), dir_name)
+            return os.path.join(new_dir_path, video_files[0])
 
         time.sleep(POLL_INTERVAL_SECONDS)
         seconds_waited += POLL_INTERVAL_SECONDS
@@ -30,18 +29,24 @@ def get_video_files(dir_path):
     return [file_name for file_name in os.listdir(dir_path) if file_name.endswith('.mp4')]
 
 
-
 if __name__ == '__main__':
+    print('running python main')
     # pretty sure there's only ever one line, but seems we have to iterate to access stdin
     for line in sys.stdin:
-        if "CREATE" in line and "ISDIR" in line:
-            new_dir_name = line.strip().split()[-1]
-            config = get_all()
+        print("line={l}".format(l=line))
+        if "ISDIR" in line:
+            print("Found create directory; line is \"{l}\"".format(l=line))
             
-            print(watch_new_dir(
-                new_dir_name,
-                config['zoom_directory_path'],
-                config['max_seconds_to_wait_for_download'],
-            ))
-            
+            line_segments = line.strip().split()
+            new_dir_name = line_segments[-1]
+            base_directory_path = line_segments[0]
+            print("new_dir_name: \"{new_dir}\"; base_dir_path: \"{base_dir}\"".format(new_dir=new_dir_name, base_dir=base_directory_path))
+
+            file_name = watch_new_dir(new_dir_name, base_directory_path, get_value('max_seconds_to_wait_for_download'))
+            file_path = os.path.join(base_directory_path, new_dir_name, file_name)
+            print("mp4 file path: \"{f}\"".format(f=file_path))
+
+            # name the Gdrive file as the directory
+            upload_video(file_path, new_dir_name)
+
             break
--- code/zoom_directory_loop.sh
+++ code/zoom_directory_loop.sh
@@ -1,4 +1,5 @@
 ZOOM_DIR_PATH=$(python3 ./read_settings.py zoom_directory_path)
+echo "inotifywait will watch $ZOOM_DIR_PATH"
 while true; do
-    inotifywait $ZOOM_DIR_PATH | python3 ./process_zoom_directory_loop.py
+    inotifywait -e create $ZOOM_DIR_PATH | python3 ./process_zoom_directory_loop.py
 done
